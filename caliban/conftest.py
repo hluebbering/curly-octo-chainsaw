87d69d3092338e2e308ca297ac71d4ae200a8c70
Hannah <luebhr17@wfu.edu>
2020-07-09 01:58:09 -0500

    caliban files :cloud:

---
+++ caliban/conftest.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Configuration for Hypothesis tests."""
+
+import os
+
+from hypothesis import Verbosity, settings
+
+settings.register_profile("ci", max_examples=1000)
+settings.register_profile("dev", max_examples=10)
+settings.register_profile("debug", max_examples=10, verbosity=Verbosity.verbose)
+settings.load_profile(os.getenv(u'HYPOTHESIS_PROFILE', 'default'))
